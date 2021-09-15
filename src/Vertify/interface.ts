342f92f5555a44e117811ba6f235e4fcbee5227b
Zhangwai <1519214533@qq.com>
2021-09-16 02:52:11 +0800

    🐛 fix: fix some ts

--- src/Vertify/interface.ts
+++ src/Vertify/interface.ts
@@ -5,22 +5,22 @@ export interface VertifyProps {
    * @description   canvas宽度
    * @default       320
    */
-  width: number;
+  width?: number;
   /**
    * @description   canvas高度
    * @default       160
    */
-  height: number;
+  height?: number;
   /**
    * @description   滑块边长
    * @default       42
    */
-  l: number;
+  l?: number;
   /**
    * @description   滑块半径
    * @default       9
    */
-  r: number;
+  r?: number;
   /**
    * @description   拼图x轴位置
    * @default       null
@@ -40,27 +40,27 @@ export interface VertifyProps {
    * @description   是否可见
    * @default       true
    */
-  visible: boolean;
+  visible?: boolean;
   /**
    * @description   滑块文本
    * @default       滑动验证
    */
-  text: string | ReactNode;
+  text?: string | ReactNode;
   /**
    * @description   滑块文本
    * @default       请再试一次
    */
-  errorText: string | ReactNode;
+  errorText?: string | ReactNode;
   /**
    * @description   刷新按钮icon, 为icon的url地址
    * @default       -
    */
-  refreshIcon: string;
+  refreshIcon?: string;
   /**
    * @description   用于获取随机图片的url地址
    * @default       https://picsum.photos/${id}/${width}/${height}, 具体参考https://picsum.photos/, 只需要实现类似接口即可
    */
-  imgUrl: string;
+  imgUrl?: string;
   /**
    * @description   验证成功回调
    * @default       ():void => {}
